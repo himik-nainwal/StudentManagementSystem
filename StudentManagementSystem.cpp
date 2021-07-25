ea94d0bace9605dc7b4f4a47e062e56fa076808b
Harshit Bhatt <harshitbhatt972@gmail.com>
2021-07-25 12:30:43 +0530

    created static method for adding and deleting details

--- StudentManagementSystem.cpp
+++ StudentManagementSystem.cpp
@@ -1,48 +1,50 @@
 #include<bits/stdc++.h>
+#include<windows.h>
 using namespace std;
 class StudentManagementSystem
 {
 public:
-	void MainMeat()
-	{
-		ShowHeading();
-		cout << endl << endl;
-		int action;
-		do {
-			ShowOptionsHomeScreen();
-			cin >> action;
-			if (action == 1)
-			{
-				//writing in file
-				cout << "Adding file" << endl;
-			} else if (action == 2)
-			{
-				//reading from file
-				cout << "Reading file" << endl;
-			} else if (action == 3)
-			{
-				//deleting student from roll no.
-				cout << "Deleting file" << endl;
-			} else if (action > 4)
-			{
-				cout << "Invalid option" << endl;
-			}
-		} while (action != 4);
+    void MainMeat()
+    {
+        ShowHeading();
+        cout << endl << endl;
+        int action;
+        do {
+            ShowOptionsHomeScreen();
+            system("Color 07");
+            cin >> action;
+            if (action == 1)
+            {
+                //writing in file
+                cout << "Adding file" << endl;
+            } else if (action == 2)
+            {
+                //reading from file
+                cout << "Reading file" << endl;
+            } else if (action == 3)
+            {
+                //deleting student from roll no.
+                cout << "Deleting file" << endl;
+            } else if (action > 4)
+            {
+                cout << "Invalid option" << endl;
+            }
+        } while (action != 4);
 
-	}
-	void ShowHeading()
-	{
-		cout << "\t\t" << "****Student Management System****" << endl;
-	}
-	void ShowOptionsHomeScreen()
-	{
-		cout << "Select one option" << endl;
-		cout << "1.Add Student Record\n" << "2.View Student Record\n" << "3.Delete Student Record\n" << "4.Quit" << endl;
-	}
+    }
+    void ShowHeading()
+    {
+        cout << "\t\t\t\t" << "****Student Management System****" << endl;
+    }
+    void ShowOptionsHomeScreen()
+    {
+        cout << "Select one option" << endl;
+        cout << "1.Add Student Record\n" << "2.View Student Record\n" << "3.Delete Student Record\n" << "4.Quit" << endl;
+    }
 };
 int main()
 {
-	StudentManagementSystem st;
-	st.MainMeat();
-	return 0;
+    StudentManagementSystem st;
+    st.MainMeat();
+    return 0;
 }
